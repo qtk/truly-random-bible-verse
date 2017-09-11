f32ec9d3c9e8396864d9cd2d696d7ccc20514c17
qtk <qtk@users.noreply.github.com>
2017-09-11 21:57:21 +0200

    added random verse logic

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+window.onload = function() {
+  var request = new XMLHttpRequest();
+  request.onload = function() {
+    // get the file contents
+    var fileContent = this.responseText;
+    // split into lines
+    var fileContentLines = fileContent.split( '\n' );
+    // get a random index (line number)
+    var randomLineIndex = Math.floor( Math.random() * fileContentLines.length );
+    // extract the value
+    var randomLine = fileContentLines[ randomLineIndex ];
 
+    // add the random line in a div
+    document.getElementById( 'verse' ).innerHTML = randomLine;
+};
+request.open( 'GET', 'https://qtk.github.io/truly-random-bible-verse/kjvdat.txt', true );
+request.send();
+}
