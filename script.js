8f70475f1fbb06fa767c641ee08e71294e40ad0c
qtk <qtk@users.noreply.github.com>
2017-09-12 17:34:04 +0200

    removed extra brace

--- script.js
+++ script.js
@@ -23,4 +23,3 @@ function getRandomIntInclusive(min, max) {
 n = getRandomIntInclusive(0, 31101).toString();
 request.open( 'GET', 'https://qtk.github.io/truly-random-bible-verse/verses/' + n + '.txt', true );
 request.send();
-}
